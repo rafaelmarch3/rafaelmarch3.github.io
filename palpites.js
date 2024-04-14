123947618208491078bcd601375be5272d91bccc
rafaelmarch3 <rafaelmarch3@gmail.com>
2024-04-14 10:12:07 -0300

    Updating table and now code has javascript for easy changes

--- palpites.js
+++ palpites.js
@@ -1,15 +1,60 @@
-guesses = {
-    March: [ "V", "E", "E"],
-    Manu: [ "V", "E", "E"],
-    Fifi : [ "V", "E", "E"]
-}
+// Number of matches and players. This will not change
+nMatches = 19;
+nPlayers = 10;
 
+// Matches. This will not change
 matches = [
-    "Grêmio (C)", "RB Bragantino (F)", "Fluminense (F)"
+    "Grêmio (C)", 
+    "RB Bragantino (F)", 
+    "Fluminense (F)", 
+    "Criciúma (C)", 
+    "Athletico-PR (F)",
+    "Vitória (C)",
+    "Flamengo (C)",
+    "Palmeiras (F)",
+    "Cruzeiro (C)",
+    "Juventude (F)",
+    "São Paulo (C)",
+    "Bahia (F)",
+    "Botafogo (C)",
+    "Fortaleza (C)",
+    "Internacional (F)",
+    "Corinthians (C)",
+    "Atlético-GO (F)",
+    "Atlético-MG (F)",
+    "Cuiabá (C)"
 ];
 
-nMatches = 3;
+// Players and guesses. This will not change
+players = ["March", "Manu", "Fifi", "Guerra", "Finho", "Saymon", "Markim", "Testa", "Poliça", "Saúde"];
+guesses = [["V", "E", "E", "V", "D", "E", "D", "E", "V", "E", "E", "V", "V", "V", "D", "E", "E", "E", "V"], 
+            ["E", "D", "V", "V", "D", "V", "D", "D", "E", "E", "V", "D", "E", "V", "D", "E", "E", "D", "V"],
+            ["V", "D", "V", "V", "D", "V", "E", "D", "V", "E", "E", "D", "V", "V", "D", "E", "E", "D", "V"],
+            ["E", "V", "V", "V", "D", "V", "E", "D", "E", "E", "E", "D", "V", "V", "D", "E", "V", "E", "V"],
+            ["E", "D", "E", "V", "D", "V", "D", "D", "V", "E", "E", "D", "V", "V", "D", "D", "E", "D", "V"],
+            ["V", "D", "E", "V", "D", "V", "D", "D", "V", "E", "V", "D", "V", "V", "D", "D", "E", "D", "V"],
+            ["V", "D", "E", "V", "D", "V", "E", "D", "E", "V", "E", "D", "V", "V", "D", "V", "E", "D", "V"],
+            ["V", "D", "V", "V", "D", "E", "D", "D", "E", "E", "E", "D", "V", "V", "D", "E", "V", "D", "V"],
+            ["E", "D", "V", "V", "D", "V", "D", "D", "E", "V", "E", "D", "E", "V", "D", "D", "V", "D", "V"],
+            ["V", "V", "E", "V", "D", "V", "D", "D", "E", "D", "E", "D", "V", "E", "D", "E", "E", "D", "V"]];
 
-players = ["March", "Manu", "Fifi"];
 
-points = ["30", "24", "26"]
+// Counting player points
+let playersPoints = ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0"];
+for(let i = 0; i < nPlayers; i++) {
+    for(let j = 0; j < nMatches; j++) {
+        if(guesses[i][j] === "V") {
+            playersPoints[i] = (parseInt(playersPoints[i]) + 3).toString();
+        } else if(guesses[i][j] === "E") {
+            playersPoints[i] = (parseInt(playersPoints[i]) + 1).toString();
+        }
+    }
+}
+console.log(playersPoints);
+
+// Match results: update during the championship
+results = ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""];
+
+// Actual points: update during the championship
+actualPoints = "0";
+
