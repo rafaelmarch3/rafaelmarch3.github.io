499b5ab26641e0a524d0bba788522aee79390895
rafaelmarch3 <rafaelmarch3@gmail.com>
2024-04-14 08:59:32 -0300

    Saving progress

---
+++ app.js
@@ -0,0 +1,7 @@
+// import palpites from "./palpites.js";
+
+// Get table container
+let tableContainer = document.querySelector(".table-container");
+
+// Reading JSON file
+console.log(palpites)
---
+++ palpites.js
@@ -0,0 +1,15 @@
+guesses = {
+    March: [ "V", "E", "E"],
+    Manu: [ "V", "E", "E"],
+    Fifi : [ "V", "E", "E"]
+}
+
+matches = [
+    "Grêmio (C)", "RB Bragantino (F)", "Fluminense (F)"
+];
+
+nMatches = 3;
+
+players = ["March", "Manu", "Fifi"];
+
+points = ["30", "24", "26"]
